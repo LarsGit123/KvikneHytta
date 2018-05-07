c15fa84a482cb979006f3e5dec7a50b617e654c3
Lars <llj@live.no>
2018-05-07 22:56:41 +0200

    first commit

---
+++ PythonStarter.sh
@@ -0,0 +1,2 @@
+sleep 10
+sudo python /usr/local/bin/pythonMQTT.py
---
+++ pythonMQTT.py
@@ -0,0 +1,48 @@
+import paho.mqtt.client as mqtt
+import base64
+import os
+import time
+topicPic="Kvikne/pic"
+topicCmd="Kvikne/cmd"
+topicMaint="Kvikne/maint"
+AT16host="178.164.99.145"
+
+def on_connect(self, client, userdata,rc):
+    print("connected" + str(rc))
+    time.sleep(5)
+    sub(client)
+    
+    
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("Received message "+ str(message.payload) + " on topic "+message.topic)
+    if message.topic==topicCmd && message.payload:
+        print("post picture")
+        postPic()
+    elif message.topic==topicMaint && message.payload:
+        client.disconnect()
+        time.sleep(5)
+        client.connect(AT16host)
+
+def postPic():
+    with open("/home/pi/webcam/snapshot.jpg", "rb") as imageFile:
+            bInString= imageFile.read()
+            byteArr=bytes(bInString)
+            client.publish(topicPic, byteArr, 0)            
+            print("publish pic on topic "+topicPic)
+            time.sleep(2)
+
+def sub(client):    
+    client.subscribe(topicCmd)
+    print("subscribed to :"+topicCmd)
+    client.subscribe(topicMaint)
+    print("subscribed to :"+topicMaint)
+        
+client=mqtt.Client("KvikneClient")
+client.on_message=on_message
+client.on_connect=on_connect
+client.connect(AT16host)
+time.sleep(5)
+
+
+client.loop_forever()
